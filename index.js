f8db7cdd261a7229f7ca4a315230e5647fef3148
frastoneme <oigoeme@gmail.com>
2024-03-24 00:45:36 +0300

    loops for and while update

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards( names, events ) {
+    let thanks = [];
+    for ( let i = 0; i < names.length; i++ ) {
+      thanks.push( `Thank you, ${names[i]}, for the wonderful ${events} gift!` )
+    }
+    return thanks;
+  }
+  
+  function countDown( start) {
+    while ( start >= 1 ) {
+      console.log( start );
+      start= start - 1;
+    }
+    console.log( start);
+  }
